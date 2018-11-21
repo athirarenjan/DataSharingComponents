6bc659cee3715b07e2d85d69ff901994cfe978f6
athirarenjan <athirarenjan8@gmail.com>
2018-11-21 16:55:19 +0530

    Initial commit

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <ul style="height:200px;overflow:scroll;">
+        <li v-for="(post, index) of posts" v-bind:key="post.id">
+          <a
+            href="javascript:;"
+            style="cursor:pointer;"
+            @click="pageSelected(index);"
+          >
+            <h3>{{ post.title }}</h3>
+          </a>
+          <p>{{ post.body }}</p>
+        </li>
+      </ul>
+      <h2>2) Data shared through Event Bus</h2>
+      i)Creating a global event bus
+    </div>
+  </div>
+</template>
+<script>
+import serverBus from "@/main";
+
+export default {
+  name: "BlogPost",
+  data() {
+    return {};
+  },
+  props: ["posts"],
+  created: function() {},
+  methods: {
+    pageSelected: function(index) {
+      alert("clicked");
+      serverBus.$emit("pageSelected", this.posts[index].body);
+    }
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <div>
+      <h1>{{ msg }}</h1>
+      <h2>Sharing data across Vuejs components</h2>
+      <h2>1) Using props-parent to child</h2>
+      <h5>API CALLS</h5>
+    </div>
+    <div><blog-post :posts="posts"></blog-post></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import BlogPost from "@/components/BlogPost";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Components In Detail"
+    };
+  },
+  components: { BlogPost },
+  created: function() {
+    var vm = this;
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        vm.posts = data;
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
